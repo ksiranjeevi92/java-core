859c2ca7e0660cd13481ea756ba363dd52036cea
ksiranjeevi92 <ksiranjeevi92@gmail.com>
2022-08-28 18:28:53 +0530

    Create new project

---
+++ src/com/cloudfirestream/Main.java
@@ -0,0 +1,13 @@
+package com.cloudfirestream;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        boolean isCar = false;
+
+        if(isCar = true){
+            System.out.println("This is not to be happen");
+        }
+    }
+}
